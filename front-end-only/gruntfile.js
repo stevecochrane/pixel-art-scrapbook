a75303adfd96133a77ce41ab8d0a98e566394f01
Steve Cochrane <steve@stevecochrane.com>
2013-10-15 16:58:09 -0700

    Restructured to make Grunt even more powerful. Grunt watch now outputs uncompressed CSS for easy reading. Default grunt command now also minifies images and HTML and outputs everything in the dist folder while preserving directory structure so it's ready to be deployed.

--- front-end-only/gruntfile.js
+++ front-end-only/gruntfile.js
@@ -6,15 +6,26 @@ module.exports = function(grunt) {
         //  Read in settings from package.json.
         pkg: grunt.file.readJSON('package.json'),
 
-        //  Compile SASS w/ Compass.
+        //  Compile SASS w/ Compass. When in dev mode, output in expanded style so the CSS is 
+        //  easy to read, and when in dist mode, output compressed (minified) for production.
         compass: {
+            dev: {
+                options: {
+                    require: ['normalize'],
+                    sassPath: 'src/sass',
+                    cssPath: 'src/css',
+                    imagesPath: 'src/img',
+                    javascriptsPath: 'src/js',
+                    outputStyle: 'expanded'
+                }
+            },
             dist: {
                 options: {
                     require: ['normalize'],
-                    sassPath: 'sass',
-                    cssPath: 'css',
-                    imagesPath: 'img',
-                    javascriptsPath: 'js',
+                    sassPath: 'src/sass',
+                    cssPath: 'dist/css',
+                    imagesPath: 'dist/img',
+                    javascriptsPath: 'dist/js',
                     outputStyle: 'compressed'
                 }
             }
@@ -25,7 +36,7 @@ module.exports = function(grunt) {
             //  Define the files to lint.
             files: [
                 'gruntfile.js',
-                'js/app.js'
+                'src/js/app.js'
             ]
         },
 
@@ -40,13 +51,13 @@ module.exports = function(grunt) {
             dist: {
                 //  The files to concatenate, in their intended order.
                 src: [
-                    'js/libs/jquery-1.9.1.js',
-                    'js/libs/handlebars-1.0.0.js',
-                    'js/libs/ember-1.0.0.js',
-                    'js/app.js'
+                    'src/js/libs/jquery-1.9.1.js',
+                    'src/js/libs/handlebars-1.0.0.js',
+                    'src/js/libs/ember-1.0.0.js',
+                    'src/js/app.js'
                 ],
                 //  The location of the resulting JS file.
-                dest: 'dist/<%= pkg.name %>.js'
+                dest: 'dist/js/<%= pkg.name %>.js'
             }
         },
 
@@ -54,8 +65,40 @@ module.exports = function(grunt) {
         uglify: {
             dist: {
                 files: {
-                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                    'dist/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
                 }
+            }
+        },
+
+        //  This will find any HTML files in the src folder, minify them, and place the 
+        //  minified versions into the dist folder, preserving directory structure.
+        //  (ex. /src/index.html will output to /dist/index.html)
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.html'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+
+        //  This will find any images in the src folder, minify them, and place the 
+        //  minified versions into the dist folder, preserving directory structure.
+        //  (ex. images in /src/img will go into /dist/img)
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'dist/'
+                }]
             }
         },
 
@@ -63,19 +106,23 @@ module.exports = function(grunt) {
         watch: {
             compass: {
                 files: ['<%= compass.dist.options.sassPath %>/*.scss'],
-                tasks: ['compass:dist']
+                tasks: ['compass:dev']
             }
         }
     });
 
     //  Load plugin(s).
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    //  Default task(s).
-    grunt.registerTask('default', ['compass', 'jshint', 'concat', 'uglify']);
+    //  Default task(s), in this case it's to build for distribution/production.
+    //  'imagemin' should be run before 'compass' so that Compass can build with the minified images.
+    //  Also it's better to test the JS with JSHint before concatenating and compressing.
+    grunt.registerTask('default', ['htmlmin', 'imagemin', 'compass', 'jshint', 'concat', 'uglify']);
 
 };
