67b3351126f44374872a05b21cd410d444c5c5cb
Steve Cochrane <steve@stevecochrane.com>
2013-10-14 21:01:07 -0700

    Moved Compass build scripting into Grunt.

--- full-functionality/gruntfile.js
+++ full-functionality/gruntfile.js
@@ -6,6 +6,20 @@ module.exports = function(grunt) {
         //  Read in settings from package.json.
         pkg: grunt.file.readJSON('package.json'),
 
+        //  Compile SASS w/ Compass.
+        compass: {
+            dist: {
+                options: {
+                    require: ['normalize'],
+                    sassPath: 'sass',
+                    cssPath: 'css',
+                    imagesPath: 'img',
+                    javascriptsPath: 'js',
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+
         //  Lint test the JavaScript. (Only the new JS for this app, frameworks not included.)
         jshint: {
             //  Define the files to lint.
@@ -43,15 +57,25 @@ module.exports = function(grunt) {
                     'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
                 }
             }
+        },
+
+        //  Watch certain files and when they change, execute some of the above plugins.
+        watch: {
+            compass: {
+                files: ['<%= compass.dist.options.sassPath %>/*.scss'],
+                tasks: ['compass:dist']
+            }
         }
     });
 
     //  Load plugin(s).
+    grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     //  Default task(s).
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('default', ['compass', 'jshint', 'concat', 'uglify']);
 
 };
